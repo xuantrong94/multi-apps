2ec5ab15561a93576cb9494f297b7a41800788fc
Nguyen Xuan Trong <trongnx@gofiber.vn>
2026-08-26 16:47:39 +0700

    chore: initial project setup with eslint, prettier, husky, commitlint

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tailwind from "eslint-plugin-tailwindcss";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    ...tailwind.configs.recommended,
+    settings: {
+      tailwindcss: {
+        config: "app/globals.css",
+        cssConfigPath: "app/globals.css",
+      },
+    },
+  },
+  {
+    rules: {
+      "tailwindcss/classnames-order": "off",
+    },
+  },
+  eslintPluginPrettierRecommended,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
